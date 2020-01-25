f468e41c8ed2b98f389647377f194559d7091fd2
Dunja Lalic <dunja.lalic@gmail.com>
2020-01-25 16:58:13 +0100

    Fix dependency errors

--- src/view/main_view.rs
+++ src/view/main_view.rs
@@ -1,5 +1,10 @@
 use crate::controller::windmill::Windmill;
-use graphics::*;
+use crate::graphics::color::{BLACK, WHITE};
+use crate::graphics::ellipse;
+use crate::graphics::ellipse::circle;
+use crate::graphics::text;
+use crate::graphics::Transformed;
+use crate::graphics::{clear, Line};
 use opengl_graphics::{GlGraphics, GlyphCache};
 use piston::input::RenderArgs;
 
@@ -14,22 +19,22 @@ const BLUE: [f32; 4] = [0.0, 0.0, 1.0, 1.0];
 impl MainView {
     pub fn render(&mut self, args: &RenderArgs, windmill: &Windmill) {
         let points = windmill.get_points();
-        let windmill_line = Line::new(color::BLACK, 0.5);
+        let windmill_line = Line::new(BLACK, 0.5);
         let glyphs = &mut self.glyphs;
 
         self.gl.draw(args.viewport(), |c, gl| {
-            clear(color::WHITE, gl);
+            clear(WHITE, gl);
             if let Some(pivot) = windmill.get_pivot() {
                 for point in points {
                     let mut point_color = BLUE;
                     if point.get_point() == pivot {
-                        point_color = color::BLACK;
+                        point_color = BLACK;
                     } else if point.is_left_of_pivot() {
                         point_color = ORANGE;
                     }
                     ellipse(
                         point_color,
-                        ellipse::circle(point.get_point().x, point.get_point().y, 4.0),
+                        circle(point.get_point().x, point.get_point().y, 4.0),
                         c.transform,
                         gl,
                     );
@@ -37,8 +42,7 @@ impl MainView {
                     let transform = c
                         .transform
                         .trans(point.get_point().x, point.get_point().y + 20.0);
-                    text(color::BLACK, 12, &count, glyphs, transform, gl)
-                        .expect("Failed to draw text");
+                    text(BLACK, 12, &count, glyphs, transform, gl).expect("Failed to draw text");
                 }
             };
             if let Some(line) = windmill.line() {
